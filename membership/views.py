b468df6864d32416deed99002e576205c971bb60
deano98 <deano_98@hotmail.com>
2022-03-29 20:42:46 +0000

    Add tdee calc to members page

--- membership/views.py
+++ membership/views.py
@@ -1,13 +1,46 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Member
+from .forms import TdeeForm
 import stripe
 
 # Create your views here.
 
 def membership(request):
-    ''' View to return the index page '''
-    return render(request, 'membership/membership.html')
+    ''' View to return the membership page '''
+    if request.method == 'POST':
+        tdee_data = request.POST
+        gender = tdee_data.get("gender")
+        weight = float(tdee_data.get("weight"))
+        height = float(tdee_data.get("height"))
+        age = float(tdee_data.get("age"))
+        activity = tdee_data.get("activity")
+        multiplier = 1
+
+        if gender == "male":
+            BMR = 66 + (13.7 * weight) + (5 * height) - (6.8 * age)
+        else:
+            BMR = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
+
+        if activity == "sedentary":
+            multiplier = 1.2
+        elif activity == "light":
+            multiplier = 1.375
+        elif activity == "moderate":
+            multiplier = 1.55
+        elif activity == "heavy":
+            multiplier = 1.725
+        else:
+            multiplier = 1.9
+
+        user_tdee = BMR * multiplier
+        print(BMR)
+        print(multiplier)
+        print(user_tdee)
+
+    return render(request, 'membership/membership.html', {
+        'tdee_form': TdeeForm(),
+    })
 
 stripe.api_key = "sk_test_51KPpqoJAp5w3wtxOBWUnJsxJqEb3uaoG7AK0cbFTyHv5uYJsSYeLm5hb2e4CQ2HCO2sPKxaaNPBwuD9KK6KHqm5H0022pEx4RX"
 
@@ -88,3 +121,6 @@ def settings(request):
             membership = False
     return render(request, 'membership/settings.html', {'membership':membership,
     'cancel_at_end':cancel_at_end})
+
+
+
